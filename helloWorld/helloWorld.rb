933cf96c2ce4701092a148b944ab17e68c5b9763
root <root@visustat.com>
2015-06-24 00:33:05 +0000

    First Ruby Script

---
+++ helloWorld/helloWorld.rb
@@ -0,0 +1,38 @@
+# William Hudgins
+# 06/23/15
+# helloWorld.rb
+#
+# My first Ruby script
+
+
+=begin 
+A 
+multi
+comment
+=end
+
+# New lines and ; both can terminate statements
+puts 'Hello World'
+puts 'I am learning Ruby, here are some notes on it:'
+puts ''
+puts 'Ruby is dynamically typed language with a strong type system.'
+puts 'PHP\'s null is Ruby\'s nil and true and false are the same.'
+puts 'Everything is an obect...there are no primative types.'
+puts 'Like Haskell, parens are optional in method calls.'
+puts 'Because everything is a nobject, all variables are references.'
+puts 'Unlike Java or PHP, there are no abstract classes or interfaces.j'
+puts 'Unlike PHP, hashes and arrays are not interchangeable.'
+puts '0, array(), and "" are true and false and nil are false.'
+puts 'Almost everything is a method call.'
+puts 'Like Java, Ruby has a garbage collector.'
+puts 'There are public, private, and protected methods.'
+puts 'rdoc is similar to javadoc.'
+puts 'Whereas java uses import and php uses include or require_once, Ruby uses require.'
+puts 'All object attributes are private.'
+puts 'Because variables are references, they don\'t have a type associated with them.'
+puts 'While poessessing a strong type system, declarations aren\'t needed because the type system is still dynamic.'
+puts 'Constructors are named initialize.'
+puts '== tests equivalance and equals?() tests identity.'
+puts 'Unlike Java, strings are mutable.'
+puts 'chars are one-letter strings.'
+
